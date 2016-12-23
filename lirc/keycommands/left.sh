62d57847b3b0e5c1122378305a2ea2fea2cbff70
Libby Miller <libby.miller@bbc.co.uk>
2016-12-23 11:51:12 +0000

    Add tiny server with js examples

--- lirc/keycommands/left.sh
+++ lirc/keycommands/left.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DISPLAY=:0.0 /usr/bin/xdotool key --window "$(xdotool search --name lirc-example | head -n 1)" Left .> /home/pi/log.txt 2>&1
+DISPLAY=:0.0 /usr/bin/xdotool key --window "$(xdotool search --name lirc-example | head -n 1)" Left > /home/pi/log.txt 2>&1
 
---
+++ lirc/keycommands/right.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DISPLAY=:0.0 /usr/bin/xdotool key --window "$(xdotool search --name lirc-example | head -n 1)" Right > /home/pi/log.txt 2>&1
+
